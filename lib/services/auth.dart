e2794ebe7ae703bbba468071e21c9a3bc737a407
lleite12 <lucassouzaleite2012@hotmail.com>
2020-06-14 15:14:51 -0300

    commit 12

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,4 +1,4 @@
-
+import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
 
 
@@ -6,10 +6,27 @@ class Auth {
   
   static String userId= "";
   
-  static Future<String> signIn(String email, String password) async {
+  static String signIn(String email, String password){
     
+    var data = [];
+
+    Response response = await Dio().request(
+      "http://ohcampeao.ddns.net/api/produto/todos",
+      options: Options(headers: {"Accept": "application/json"})
+    );
+    
+    var produto = response.data["resultData"];
+
+    print(produto);
+
+    for(Map<String,dynamic> item in produto){
+      data.add(Product.fromJson(item));
+    }        
+
+    return true;
+
     userId = "Guilherme";
-    return "Guilherme";
+    return "NOK";
   }
 
   static Future<String> signUp(String cpf, String password, String name, String phone) async {
